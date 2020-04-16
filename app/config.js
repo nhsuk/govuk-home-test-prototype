75d241d6a55c46462c7ae4e0f08c95cc9c482c4c
AdamChrimes <iChrimes@live.co.uk>
2020-04-16 10:12:27 +0100

    create and populate the start page

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Register a COVID-19 home test',
 
   // Default port that prototype runs on
   port: '3000',
