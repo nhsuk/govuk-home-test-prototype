ebb0af555704eba00ca82ea403b2c9b0c1fad31a
AdamChrimes <iChrimes@live.co.uk>
2020-04-26 11:48:06 +0100

    split out the regional check into v3

--- app/routes.js
+++ app/routes.js
@@ -1,18 +1,29 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
-// Test kit type routing
+// Version 2 - Test kit type routing (Country)
 router.post('/v2/action/test-kit-type', function (req, res) {
   var testKit = req.session.data['test-kit']
 
+  if (testKit == "By requesting a home test kit"){
+    res.redirect('https://www.gov.uk')
+  } else {
+    res.redirect('/v2/ask-for-a-coronavirus-test/test-centre')
+  }
+
+})
+
+// Version 2 - Test kit type routing (Region)
+router.post('/v3/action/test-kit-type', function (req, res) {
+  var testKit = req.session.data['test-kit']
+
   if (testKit == "By visiting a regional test site in England"){
-    res.redirect('/v2/ask-for-a-coronavirus-test/test-kit-type-regions')
+    res.redirect('/v3/ask-for-a-coronavirus-test/test-kit-type-regions')
   }
   else if (testKit == "By requesting a home test kit"){
     res.redirect('https://www.gov.uk')
   } else {
-    res.redirect('/v2/ask-for-a-coronavirus-test/test-centre')
+    res.redirect('/v3/ask-for-a-coronavirus-test/test-centre')
   }
 
 })
